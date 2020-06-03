431bdc3991b75dfd7538d8c81b46323b0e765c34
vpasport <pasportnikov2000@gmail.com>
2020-06-04 02:54:38 +0800

    add graphs

---
+++ static/scripts/graphs.js
@@ -0,0 +1,163 @@
+"use strict";
+
+// const socket = io( "http://localhost:3000" );
+let q = [];
+let N;
+
+function rotatePoint( turn_, turnAround, angel ){
+  let turn, turned;
+
+  turn = [ ...turn_ ];
+  turned = [];
+  angel *= Math.PI / 180;
+  turn[0] -= turnAround[0];
+  turn[1] -= turnAround[1];
+  turned.push( turn[0] * Math.cos( angel ) - turn[1] * Math.sin( angel ) );
+  turned.push( turn[0] * Math.sin( angel ) + turn[1] * Math.cos( angel ) );
+  turned[0] += turnAround[0];
+  turned[1] += turnAround[1];
+
+  return turned;
+}
+
+class Graph{
+  constructor( el ){
+    this.el = el;
+    this.width = el.offsetWidth;
+    this.height = el.offsetHeight;
+    this.paper = new Raphael( el, this.width, this.height );
+  }
+
+  drawPoints( n ){
+    const angle = 360 / n;
+    const center = [ this.width / 2, this.height / 2 ];
+    const point = [ this.width / 2, 100 ];
+
+    this.paper.clear();
+    this.rotatedPoints = [];
+
+    for( let i = 0; i < 360; i += angle )
+      this.rotatedPoints.push( rotatePoint( point, center, i ) );
+
+    for( let [ i, point ] of Object.entries( this.rotatedPoints ) ){
+      this.paper.circle( point[0], point[1], 10 ).attr( {
+          "stroke" : "#0062cc"
+      } );
+      this.paper.text( point[0], point[1], i ).attr( {
+        "font-size" : "18px",
+        "fill" : "#0062cc",
+        "stroke" : null
+     } );
+    }
+  }
+
+  drawLine( path ){
+    let st = "M";
+
+    if( this.path !== undefined )
+      this.path.remove();
+
+    for( let [ i, el ] of Object.entries( path ) ){
+      st += this.rotatedPoints[ el ];
+
+      if( i < path.length - 1 ) st += "L";
+    }
+
+    this.path = this.paper.path( st ).attr( {
+      "stroke" : "#0062cc"
+  } );
+  }
+}
+
+class Chart{
+  constructor( el, maxPathLength, maxN ){
+    this.el = el;
+    this.width = el.offsetWidth;
+    this.height = el.offsetHeight;
+    this.paper = new Raphael( el, this.width, this.height );
+    this.maxPathLength = maxPathLength;
+    this.maxN = maxN;
+  }
+
+  init( pathLength, i ){
+    this.paper.clear();
+
+    this.oldPoint = [
+      this.width / this.maxN * i,
+      this.height - this.height / this.maxPathLength * pathLength
+    ];
+  }
+
+  drawLine( pathLength, i ){
+    const newPoint = [
+      this.width / this.maxN * i,
+      this.height - this.height / this.maxPathLength * pathLength
+    ];
+
+    this.paper.path(
+      `M${this.oldPoint[0]},${this.oldPoint[1]}
+      L${newPoint[0]},${newPoint[1]}`
+    ).attr( {
+      "stroke" : "#0062cc"
+  } );
+
+    this.oldPoint = newPoint;
+  }
+}
+
+function getMaxSum( data ){
+  let sum = 0;
+  let gmax = 0;
+
+  for( let line of data ){
+    let max = 0;
+
+    for( let el of line )
+      max = el > max ? el : max;
+
+    sum += max;
+    gmax = max > gmax ? max : gmax;
+  }
+
+  sum += gmax;
+
+  return sum;
+}
+
+function index(){
+  const note = document.getElementById( "note" );
+  // const graph = new Graph( note );
+
+  // document.getElementById( "btn" ).addEventListener( "click", go );
+
+  // socket.on( "/api/annealing", data => {
+  //   const { event: e, path, pathLength, i } = JSON.parse( data );
+
+  //   if( e === "init" ) chart.init( pathLength, i );
+  //   else chart.drawLine( pathLength, i );
+
+  //   if( e === "end" ) console.log( `path: ${path}, pathLength: ${pathLength}` );
+  // } );
+
+  q = [
+    [ 1, 2, 2, 2 ],
+    [ 1, 2, 2 ],
+    [ 1, 2 ],
+    [ 2 ]
+  ];
+  N = 100;
+  const max = getMaxSum( q );
+  // const chart = new Chart( note, max, N );
+
+  // Generator
+  // for( let i = 0; i < 9; i++ ){
+  //   const w = [];
+  //
+  //   for( let j = i + 1; j < 10; j++ )
+  //     w.push( Math.floor( Math.random() * 10 ) + 1 );
+  //
+  //   q.push( w );
+  // }
+}
+
+window.addEventListener( "load", index );
---
+++ static/scripts/scripts.js
@@ -0,0 +1,179 @@
+$(document).ready( () => {
+    let socket = io()
+
+    $( "#mf" ).click( (e) => {
+        let bt = $( "#mf" )
+
+        if( bt.attr( "class" ) == "btn btn-secondary inputbt" ){
+            bt.attr( "class", "btn btn-primary inputbt" )
+            $( "#ff" ).attr( "class", "btn btn-secondary inputbt" )
+            $( "#inputsettings" ).attr( "hidden", false )
+        }
+    } )
+
+    $( "#ff" ).click( (e) => {
+        let bt = $( "#ff" )
+
+        if( bt.attr( "class" ) == "btn btn-secondary inputbt" ){
+            bt.attr( "class", "btn btn-primary inputbt" )
+            $( "#mf" ).attr( "class", "btn btn-secondary inputbt" )
+            $( "#inputsettings" ).attr( "hidden", true )
+        }
+    } )
+
+    $( '#stginp' ).change( (e) => {
+        $( "#alert" ).find( "p" ).remove();
+        $( "#alert" ).attr( "hidden", true )
+
+        let tb = $( '#inputtb' )
+        let size = $( '#stginp' ).val()
+        
+        if( size > 15 ){
+            let bt = $( "#ff" )
+
+            bt.attr( "class", "btn btn-primary inputbt" )
+            $( "#mf" ).attr( "class", "btn btn-secondary inputbt" )
+            $( "#inputsettings" ).attr( "hidden", true )
+            $( '#stginp' ).val( 0 )
+            return false
+        }
+        
+        if( size < 2 ){
+            $( "#alert" ).append( $( '<p>', {
+                "text" : "Invalid matrix dimensions"
+            } ) )
+            $( "#alert" ).attr( "hidden", false )
+            return false
+        }
+
+        tb.find( "tr" ).remove();
+        let val = 0
+
+        for( let i = 0; i < size; i++ ){
+            let tr = $( "<tr>" );
+            for( let j = 0; j < size; j++ ){
+                let td = $( "<td>" );
+                
+                val = 0
+
+                td.append( $( '<input>', {
+                    "id" : i + "_" + j, 
+                    "type" : "number",
+                    "class" : "form-control inps",
+                    "placeholder" : val,
+                    "value" : val
+                } ) )
+
+                if( j == i ) td.find( "input" ).attr( "readonly", true )
+
+                let input = td.find( "input" )
+                input.change( (e) => {
+                    let id = input.attr('id').split( "_" )
+                    let newId = id[1] + "_" + id[0]
+                    $( '#' + newId ).val( input.val() )
+                } )
+
+                tr.append( td )
+            }
+            tb.append( tr )
+        }
+
+        $( "#calculate" ).attr( "hidden", false )
+    } )
+
+
+    $( "#calculate" ).click( (e) => {
+        $( "#alert" ).find( "p" ).remove();
+        $( "#alert" ).attr( "hidden", true )
+        
+        let tmin = parseInt( $( '#tmin' ).val() )
+        if( tmin < 0 || isNaN( tmin ) ){
+            $( "#alert" ).append( $( '<p>', {
+                "text" : "Minimum temperature is not filled correctly"
+            } ) )
+            $( "#alert" ).attr( "hidden", false )
+            return false
+        }
+
+        let tmax = parseInt( $( '#tmax' ).val() )
+        if( tmax < 0 || isNaN( tmax ) ){
+            $( "#alert" ).append( $( '<p>', {
+                "text" : "Maximum temperature is not filled correctly"
+            } ) )
+            $( "#alert" ).attr( "hidden", false )
+            return false
+        }
+
+        let n = parseInt( $( '#steps' ).val() )
+        if( n < 1 || isNaN( n ) ){
+            $( "#alert" ).append( $( '<p>', {
+                "text" : "The number of steps is filled incorrectly"
+            } ) )
+            $( "#alert" ).attr( "hidden", false )
+            return false
+        }
+
+        let size = $( '#stginp' ).val()
+        let id
+
+        let data = []
+
+        for( let i = 0; i < size - 1; i++ ){
+            let d = []
+            for( let j = i + 1; j < size; j++ ){
+                id = i + "_" + j
+                let val = parseInt( $( "#" + id ).val() )
+                
+                if( val < 1 ){
+                    $( "#alert" ).append( $( '<p>', {
+                        "text" : "Weight cannot be less than zero"
+                    } ) )
+                    $( "#alert" ).attr( "hidden", false )
+                    return false
+                }
+
+                d.push( val )
+            }
+            data.push( d ) 
+        }
+
+        socket.emit( "/api/annealing", JSON.stringify(
+            {
+                data,
+                Tmin : tmin,
+                Tmax : tmax,
+                N : n,
+            }
+        ) )
+
+        $( "#settings" ).attr( "hidden", true )
+        $( "#graphs" ).attr( "hidden", false )
+
+        let graph = new Graph( document.getElementById( "graph" ) )
+        graph.drawPoints( size )
+
+        let chart = new Chart( document.getElementById( "chart" ), getMaxSum( data ), n )
+
+        socket.on( "/api/annealing", data => {
+            const { event: e, path, pathLength, Ti, i } = JSON.parse( data ) 
+    
+            if( e === "init" ) chart.init( pathLength, i );
+            if( e === "process" ){
+                graph.drawLine( path )
+                chart.drawLine( pathLength, i );
+                $( "#temp" ).text( "Temp: " + Ti )
+                $( "#step" ).text( "Step: " + i )
+            }    
+    
+            console.log( data )
+        } )
+    } )
+
+    $( "#settingsbt" ).click( (e) => {
+        $( "#settings" ).attr( "hidden", false )
+        $( "#graphs" ).attr( "hidden", true )
+
+        $( "#graph" ).find( "svg" ).remove()
+        $( "#chart" ).find( "svg" ).remove()
+    } )
+} )
